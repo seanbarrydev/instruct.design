e73d0879c90ef444159171f7d33316c76c8439d2
Sean Barry <seanbarry@Seans-iMac.lan>
2021-08-20 13:26:12 -0500

    Added resume page

---
+++ pages/resume.vue
@@ -0,0 +1,45 @@
+<template>
+<iframe src="Sean Barry Resume.pdf"></iframe>
+</template>
+<script>
+export default {
+  head: {
+    title: "Sean Barry's Resume",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Sean Barry's Resume",
+      },
+      {
+        hid: "og:description",
+        name: "og:description",
+        content: "Sean Barry's Resume",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "Sean Barry's Resume",
+      },
+      {
+        hid: "og:site_name",
+        name: "og:site_name",
+        content: "instruct.design",
+      },
+    ],
+    link: [
+      {
+        hid: "canonical",
+        rel: "canonical",
+        href: `https://instruct.design/resume`,
+      },
+    ],
+  },
+};
+</script>
+<style>
+iframe {
+    width: 100%;
+    height: 100vh;
+}
+</style>
