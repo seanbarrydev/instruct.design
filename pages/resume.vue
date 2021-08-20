899ae6eeda0af23ed821ed78d7b1fcdd615f6114
Sean Barry <seanbarry@Seans-iMac.lan>
2021-08-20 12:28:23 -0500

    Addded metadata to head for resume page

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,6 +1,42 @@
 <template>
 <iframe src="/resume.pdf"></iframe>
 </template>
+<script>
+export default {
+  head: {
+    title: "Sean Barry's Resume",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Sean Barry's Resume",
+      },
+      {
+        hid: "og:description",
+        name: "og:description",
+        content: "Sean Barry's Resume",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "Resume",
+      },
+      {
+        hid: "og:site_name",
+        name: "og:site_name",
+        content: "instruct.design",
+      },
+    ],
+    link: [
+      {
+        hid: "canonical",
+        rel: "canonical",
+        href: `https://instruct.design/resume`,
+      },
+    ],
+  },
+};
+</script>
 <style>
 iframe {
     width: 100%;
