e5d4977d3c45bf17297009e77ccccb79cefc647f
Sean <45640866+seanbarrydev@users.noreply.github.com>
2026-06-01 20:59:10 -0500

    fix image format

--- components/AvatarIntro.vue
+++ components/AvatarIntro.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="relative z-10 mb-8 md:mb-2">
-    <div class="flex text-base max-w-prose lg:max-w-none space-x-8 mb-12">
-      <NuxtImg src="~/assets/img/sb-1296.jpg" sizes="240" format="webp" />
+    <div class="relative z-10 mb-8 md:mb-2">
+        <div class="flex text-base max-w-prose lg:max-w-none space-x-8 mb-12">
+          <img
+            class="flex-items w-32 h-32 rounded-full border-2 border-cranberry p-1"
+            :src="require(`~/assets/img/sb-headshot-300px.png?{sizes:[240],format:'webp'}`)"
+            alt="sean barry headshot"
+          />
 
-      <div class="align-middle">
-        <h2
-          class="text-cranberry-600 dark:text-cranberry-400 font-semibold uppercase text-lg"
-        >
-          About
-        </h2>
-        <p
-          class="mt-2 text-4xl font-extrabold sm:text-5xl tracking-tight min-h-em1"
-        >
-          <vue-typed-js
+          <div class="align-middle">
+            <h2
+              class="text-cranberry-600 dark:text-cranberry-400 font-semibold uppercase text-lg"
+            >
+              About
+            </h2>
+            <p class="mt-2 text-4xl font-extrabold sm:text-5xl tracking-tight min-h-em1">
+              <vue-typed-js
             :strings="pageTitleStrings"
             :typeSpeed="20"
             :showCursor="false"
             :contentType="'html'"
-          >
-            <span class="typing"></span>
-          </vue-typed-js>
-        </p>
+            >
+              <span class="typing"></span>
+            </vue-typed-js>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      pageTitleStrings: ["Hi, I'm Sean<span class='text-cranberry'>.</span>"],
-    };
-  },
-};
+  export default {
+    data() {
+      return {
+        pageTitleStrings: ["Hi, I'm Sean<span class='text-cranberry'>.</span>"]
+      }
+    },
+  }
 </script>
